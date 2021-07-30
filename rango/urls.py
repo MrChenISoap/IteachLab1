2d0859b7268b46bee2f380860bbbd3c8c27649c5
Hongyu Chen <2531986C@student.gla.ac.uk>
2021-07-30 05:05:08 +0100

    ch7: finish exercise

--- rango/urls.py
+++ rango/urls.py
@@ -7,4 +7,5 @@ urlpatterns =[
     path('about/',views.about,name='about'),
     path('category/<slug:category_name_slug>/',views.show_category, name='show_category'),
     path('add_category/', views.add_Category, name='add_category'),
+    path('category/<slug:category_name_slug>/add_page/', views.add_Page, name='add_page')
 ]
